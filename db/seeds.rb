e4b139bb465c6a1a027fef627ea18b7f96961760
Louis Muhammad <louis.muhammad@gmail.com>
2014-05-25 16:59:12 -0400

    added project model and rspec test

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Project.delete_all
+
+project1 = Project.create(name: "Start-Up Project",
+                              description: "A description of a start-up project",
+                              target_pledge_amount: 100.00,
+                              pledging_ends_on: 1.day.from_now,
+                              website: "http://project-a.com")
+
+    project2 = Project.create(name: "Muslim Study Abroad Project",
+                              description: "People wanting to travel abroad to learn and study",
+                              target_pledge_amount: 500.00,
+                              pledging_ends_on: 10.days.from_now,
+                              website: "http://muslimstudyabraod.com")
+
+    project3 = Project.create(name: "Muslim Study Abroad Project",
+                              description: "People wanting to travel abroad to learn and study",
+                              target_pledge_amount: 500.00,
+                              pledging_ends_on: 10.days.from_now,
+                              website: "http://muslimstudyabraod.com")
+
+    puts "Success: Project data loaded"
--- spec/features/list_projects_spec.rb
+++ spec/features/list_projects_spec.rb
@@ -3,13 +3,36 @@ require 'spec_helper'
 describe "Viewing the list of projects" do
 
   it "shows three projects names" do
+    project1 = Project.create(name: "Start-Up Project",
+                              description: "A description of a start-up project",
+                              target_pledge_amount: 100.00,
+                              pledging_ends_on: 1.day.from_now,
+                              website: "http://project-a.com")
+
+    project2 = Project.create(name: "Muslim Study Abroad Project",
+                              description: "People wanting to travel abroad to learn and study",
+                              target_pledge_amount: 500.00,
+                              pledging_ends_on: 10.days.from_now,
+                              website: "http://muslimstudyabraod.com")
+
+    project3 = Project.create(name: "Muslim Study Abroad Project",
+                              description: "People wanting to travel abroad to learn and study",
+                              target_pledge_amount: 500.00,
+                              pledging_ends_on: 10.days.from_now,
+                              website: "http://muslimstudyabraod.com")
+
+
     visit projects_url
 
     expect(page).to have_text("3 Projects")
-    expect(page).to have_text("Project A")
-    expect(page).to have_text("Project B")
-    expect(page).to have_text("Project C")
-
+    expect(page).to have_text(project1.name)
+    expect(page).to have_text(project2.name)
+    expect(page).to have_text(project3.name)
+    
+    expect(page).to have_text("$100.00")
+    expect(page).to have_text(project1.target_pledge_amount)
+    expect(page).to have_text(project1.pledging_ends_on)
+    expect(page).to have_text(project1.website)
   end
 
 end
