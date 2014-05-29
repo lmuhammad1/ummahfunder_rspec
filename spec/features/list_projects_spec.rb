6f2b30a2686ad6d79455a566482aabb0c8e7bdf1
Louis Muhammad <louis.muhammad@gmail.com>
2014-05-29 11:29:55 -0400

    Add accepting_pledges method in project.rb to only show live projects in the controller

--- spec/features/list_projects_spec.rb
+++ spec/features/list_projects_spec.rb
@@ -1,37 +1,45 @@
+require 'spec_helper'
 
 describe "Viewing the list of projects" do
 
-  it "shows three projects names" do
-    project1 = Project.create(name: "Start-Up Project",
+  it "shows the projects stored in the database" do
+    projectA = Project.create(name: "Start-Up Project",
                               description: "A description of a start-up project",
                               target_pledge_amount: 100.00,
                               pledging_ends_on: 1.day.from_now,
                               website: "http://project-a.com")
 
-    project2 = Project.create(name: "Muslim Study Abroad Project",
-                              description: "People wanting to travel abroad to learn and study",
-                              target_pledge_amount: 500.00,
-                              pledging_ends_on: 10.days.from_now,
-                              website: "http://muslimstudyabraod.com")
+    projectB = Project.create(name: "Community Project",
+                              description: "A description of a community project",
+                              target_pledge_amount: 200.00,
+                              pledging_ends_on: 1.week.from_now,
+                              website: "http://project-b.com")
 
-    project3 = Project.create(name: "Muslim Study Abroad Project",
-                              description: "People wanting to travel abroad to learn and study",
-                              target_pledge_amount: 500.00,
-                              pledging_ends_on: 10.days.from_now,
-                              website: "http://muslimstudyabraod.com")
-
+    projectC = Project.create(name: "Personal Project",
+                              description: "A description of a person project",
+                              target_pledge_amount: 300.00,
+                              pledging_ends_on: 1.month.from_now,
+                              website: "http://project-c.com")
 
     visit projects_url
 
     expect(page).to have_text("3 Projects")
-    expect(page).to have_text(project1.name)
-    expect(page).to have_text(project2.name)
-    expect(page).to have_text(project3.name)
-    
+    expect(page).to have_text(projectA.name)
+    expect(page).to have_text(projectB.name)
+    expect(page).to have_text(projectC.name)
+
+    expect(page).to have_text(projectA.description)
     expect(page).to have_text("$100.00")
-    expect(page).to have_text(project1.description)
     expect(page).to have_text("1 day remaining")
-    expect(page).to have_text(project1.website)
+    expect(page).to have_text(projectA.website)
   end
 
+  it "does not show a project that is no longer accepting pledges" do
+    project = Project.new(project_attributes(pledging_ends_on: 1.day.ago))
+    project.save
+
+    visit projects_path
+
+    expect(page).not_to have_text(project.name)
+  end
 end
