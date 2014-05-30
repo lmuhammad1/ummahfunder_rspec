888ebd6cd2484653c2d8d03919650ce026d159b4
Louis Muhammad <louis.muhammad@gmail.com>
2014-05-30 09:38:35 -0400

    Added validation rules for project.rb

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,19 @@
 class Project < ActiveRecord::Base
+  validates :name, :description, presence: true
+  validates :description, length: { maximum: 500 }
+  validates :target_pledge_amount, numericality: { greater_than: 0 }
+
+  validates :image_file_name, allow_blank: true, format: {
+    with:    /\w+.(gif|jpg|png)\z/i,
+    message: "must reference a GIF, JPG, or PNG image"
+  }
+  validates :website, format: {
+    with: /https?:\/\/[\S]+\b/i,
+    message: "must reference a valid URL"
+  } 
+
+
+
   def self.accepting_pledges
     where("pledging_ends_on >= ?", Time.now).order("pledging_ends_on asc")
   end
